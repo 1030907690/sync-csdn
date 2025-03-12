9b4335d8b29b656c3e848db48a3a740002a06929
zzq <1030907690@qq.com>
2025-03-12 09:46:46 +0800

    feat sync

--- sync_csdn.py
+++ sync_csdn.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-
 from selenium.webdriver.chrome.webdriver import WebDriver
+import time
+
 
 def create_driver():
     options = webdriver.ChromeOptions()
@@ -11,17 +12,21 @@ def create_driver():
     options.add_argument(r"user-data-dir=C:\Users\Administrator\AppData\Local\Google\Chrome\User Data")  # 浏览器路径
     driver = webdriver.Chrome(options=options)
     return driver
+
+
 def start_page(driver):
     driver.get("https://mp.csdn.net/mp_blog/manage/article")
     return driver
 
-def click_visible(driver:WebDriver):
+
+def click_visible(driver: WebDriver):
     driver.implicitly_wait(15)
     driver.find_element(By.XPATH,
                         "/html/body/div[2]/div/div/div/div[2]/section/div/div[1]/div/section/section/main/div/div/div/div/div/div/div[1]/div/ul/li[2]/span").click()
 
 
-def article_list(driver:WebDriver):
+def article_list(driver: WebDriver):
+    time.sleep(3)
     driver.implicitly_wait(15)
     article_divs = driver.find_elements(By.XPATH,
                                         "/html/body/div[2]/div/div/div/div[2]/section/div/div[1]/div/section/section/main/div/div/div/div/div/div/div[3]/div[2]/div[@class='article-list-item-mp']")
@@ -32,21 +37,29 @@ def article_list(driver:WebDriver):
         a_text = a_tag.text
         p2 = article_div.find_element(By.XPATH, "div[@class=\"list-item-mp-right\"]/div/p[2]")
 
-        print(a_href + " "+p2.text.split(" ")[0] + " " + a_text)
+        print(a_href + " " + p2.text.split(" ")[0] + " " + a_text)
         # print(a_tag.text)
 
-def next_page(driver:WebDriver):
+
+def next_page(driver: WebDriver):
     driver.implicitly_wait(15)
-    driver.find_element(By.XPATH,"/html/body/div[2]/div/div/div/div[2]/section/div/div[1]/div/section/section/main/div/div/div/div/div/div/div[4]/div/button[2]").click()
+    next_btn = driver.find_element(By.XPATH,
+                                   "/html/body/div[2]/div/div/div/div[2]/section/div/div[1]/div/section/section/main/div/div/div/div/div/div/div[4]/div/button[2]")
+    btn_disabled = next_btn.get_attribute("disabled")
+    print("btn_disabled " ,btn_disabled)
+    if next_btn is not None and btn_disabled is None:
+        next_btn.click()
+        return True
+    return False
+
 
 if __name__ == '__main__':
     driver = create_driver()
     start_page(driver)
     click_visible(driver)
     article_list(driver)
-
-    next_page(driver)
-    article_list(driver)
+    while next_page(driver):
+        article_list(driver)
 
     input("按任意键结束")
     # assert "Python" in driver.title
