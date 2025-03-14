b186fcd353d5c9a0ab2320f3ed07cfd9d38913b3
zzq <1030907690@qq.com>
2025-03-14 09:06:48 +0800

    feat 博客

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ conda create -n selenium python=3.10
 conda activate selenium
 pip install selenium
 pip install pydirectinput
+pip install pyperclip
 ```
--- sync_csdn.py
+++ sync_csdn.py
@@ -5,8 +5,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
 import time
 import pydirectinput
+import pyperclip
 
 article_list = []
+
 
 def create_driver():
     options = webdriver.ChromeOptions()
@@ -41,7 +43,7 @@ def get_article_list(driver: WebDriver):
         date = p2.text.split(" ")[0]
         print(a_href + " " + date + " " + a_text)
 
-        article_list.append({"href":a_href,"date":date,"title":a_text})
+        article_list.append({"href": a_href, "date": date, "title": a_text})
         # 模拟CTRL + enter键
         # a_tag.send_keys(Keys.CONTROL + Keys.ENTER)
         # print(a_tag.text)
@@ -52,33 +54,54 @@ def next_page(driver: WebDriver):
     next_btn = driver.find_element(By.XPATH,
                                    "/html/body/div[2]/div/div/div/div[2]/section/div/div[1]/div/section/section/main/div/div/div/div/div/div/div[4]/div/button[2]")
     btn_disabled = next_btn.get_attribute("disabled")
-    print("btn_disabled " ,btn_disabled)
+    print("btn_disabled ", btn_disabled)
     if next_btn is not None and btn_disabled is None:
         next_btn.click()
         return True
     return False
 
 
-
-def open_new_tag(driver: WebDriver,url:str):
-    #切换标签页
+def open_new_tag(driver: WebDriver, url: str):
+    # 切换标签页
     # driver.switch_to.window(driver.window_handles[0])
-    print("打开URL标签页:"+url)
-    driver.execute_script('window.open("'+url+'")')
+    print("打开URL标签页:" + url)
+    driver.execute_script('window.open("' + url + '")')
+    time.sleep(3)
+    print("driver.window_handles ", driver.window_handles)
+    driver.switch_to.window(driver.window_handles[len(driver.window_handles) - 1])
 
 
-def click_editor_area(driver: WebDriver):
-    editor = driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[2]/div/div[2]/div[1]/div[@class=\"editor\"]")
-    if editor:
-        editor.click()
-    driver.implicitly_wait(15)
+def full_selected_copy():
+    print("全选")
     pydirectinput.keyDown("ctrl")
-    time.sleep(0.01)
     pydirectinput.keyDown("a")
-    time.sleep(1)
+    time.sleep(2)
     pydirectinput.keyUp("a")
     pydirectinput.keyUp("ctrl")
 
+    pydirectinput.keyDown("ctrl")
+    pydirectinput.keyDown("c")
+    print("复制完成")
+    time.sleep(0.01)
+    pydirectinput.keyUp("c")
+    pydirectinput.keyUp("ctrl")
+
+
+def click_editor_area(driver: WebDriver, article):
+    if article['href'].find("/md/") >= 0:
+        editor = driver.find_element(By.XPATH,
+                                     "/html/body/div[1]/div[1]/div[2]/div/div[2]/div[1]/div[@class=\"editor\"]")
+        if editor:
+            editor.click()
+
+        driver.implicitly_wait(15)
+        full_selected_copy()
+
+
+def copy_to_md(article):
+    content = pyperclip.paste()
+    with open(article['date'] + "-" + article['title'] + '.md', 'w', encoding='UTF-8') as file:
+        file.write(content)
 
 
 if __name__ == '__main__':
@@ -90,15 +113,10 @@ if __name__ == '__main__':
     # while next_page(driver):
     #     article_list(driver)
 
-    open_new_tag(driver, article_list[0]['href'])
-    click_editor_area(driver)
-
+    for article in article_list:
+        open_new_tag(driver, article['href'])
+        click_editor_area(driver, article)
+        copy_to_md(article)
 
     input("按任意键结束")
-    # assert "Python" in driver.title
-    # elem = driver.find_element(By.NAME, "q")
-    # elem.clear()
-    # elem.send_keys("pycon")
-    # elem.send_keys(Keys.RETURN)
-    # assert "No results found." not in driver.page_source
-    # driver.close()
+    driver.close()
