85246aabf018cc35feeb15c72228f32bbbd657cf
zhouzhongqing <zhouzhongqing@qq.com>
2025-03-16 11:37:55 +0800

    feat sync

--- sync_csdn.py
+++ sync_csdn.py
@@ -6,15 +6,19 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 import time
 import pydirectinput
 import pyperclip
+from selenium.webdriver.chrome.service import Service
+import os
 
 article_list = []
 
 
 def create_driver():
+    driver_path = "C:/Users/Administrator/.cache/selenium/chromedriver/win64/134.0.6998.88/chromedriver.exe";
     options = webdriver.ChromeOptions()
     # https://blog.csdn.net/weixin_43407092/article/details/97128833 登录保持
     options.add_argument(r"user-data-dir=C:\Users\Administrator\AppData\Local\Google\Chrome\User Data")  # 浏览器路径
-    driver = webdriver.Chrome(options=options)
+    service: Service = Service(executable_path=driver_path)
+    driver = webdriver.Chrome(options=options, service=service)
     return driver
 
 
@@ -27,6 +31,7 @@ def click_visible(driver: WebDriver):
     driver.implicitly_wait(15)
     driver.find_element(By.XPATH,
                         "/html/body/div[2]/div/div/div/div[2]/section/div/div[1]/div/section/section/main/div/div/div/div/div/div/div[1]/div/ul/li[2]/span").click()
+    time.sleep(3)
 
 
 def get_article_list(driver: WebDriver):
@@ -50,6 +55,7 @@ def get_article_list(driver: WebDriver):
 
 
 def next_page(driver: WebDriver):
+    time.sleep(2)
     driver.implicitly_wait(15)
     next_btn = driver.find_element(By.XPATH,
                                    "/html/body/div[2]/div/div/div/div[2]/section/div/div[1]/div/section/section/main/div/div/div/div/div/div/div[4]/div/button[2]")
@@ -88,21 +94,27 @@ def full_selected_copy():
 
 
 def click_editor_area(driver: WebDriver, article):
+    time.sleep(3)
     driver.implicitly_wait(15)
     if article['href'].find("/md/") >= 0:
-        editor = driver.find_element(By.XPATH,
-                                     "/html/body/div[1]/div[1]/div[2]/div/div[2]/div[1]/div[@class=\"editor\"]")
-        if editor:
-            editor.click()
+        # editor = driver.find_element(By.XPATH,
+        #                              "/html/body/div[1]/div[1]/div[2]/div/div[2]/div[1]/div[@class=\"editor\"]")
+        # if editor:
+        #     editor.click()
+        print("md 文档")
+    else:
+        pydirectinput.press('tab')
 
-        time.sleep(2)
-        full_selected_copy()
+    full_selected_copy()
 
 
-def copy_to_md(article):
+def copy_to_md(article, page):
     content = posts_template(article['title']) + pyperclip.paste()
     md_file_name = article['date'] + "-" + article['title']
-    with open(special_char_replace(md_file_name) + '.md', 'w', encoding='UTF-8') as file:
+    folder = "article/" + str(page) + "/"
+    if os.path.exists(folder) is False:
+        os.mkdir(folder)
+    with open(folder + special_char_replace(md_file_name) + '.md', 'w', encoding='UTF-8') as file:
         file.write(content)
 
 
@@ -111,8 +123,8 @@ def posts_template(title: str):
         , "layout:\t\t\t\t\tpost" + "\n"
         , "title:\t\t\t\t\t\"" + title + "\"\n"
         , "author:\t\t\t\t\tZhou Zhongqing" + "\n"
-        , "header-style:\t\t\t\ttext"+ "\n"
-        , "catalog:\t\t\t\t\ttrue"+ "\n"
+        , "header-style:\t\t\t\ttext" + "\n"
+        , "catalog:\t\t\t\t\ttrue" + "\n"
         , "tags:" + "\n"
         , "\t\t- Web" + "\n"
         , "\t\t- JavaScript" + "\n"
@@ -131,9 +143,15 @@ def special_char_replace(name: str):
     return name_str
 
 
-def  test_data():
-    article_list.append({"href": "https://editor.csdn.net/md/?articleId=139380339", "date": "2024-06-01",
-                         "title": "nuxt创建项目失败，Error: Failed to download template from registry: Failed to download https:\\//raw.githubus"})
+def test_data():
+    # article_list.append({"href": "https://editor.csdn.net/md/?articleId=139380339", "date": "2024-06-01",
+    #                      "title": "nuxt创建项目失败，Error: Failed to download template from registry: Failed to download https:\\//raw.githubus"})
+
+    article_list.append({
+        "href": "https://mp.csdn.net/mp_blog/creation/editor/46463283",
+        "date": "2015-06-11",
+        "title": "redhat钰硕ar8151网卡配置（驱动安装）教程"
+    })
 
 
 if __name__ == '__main__':
@@ -141,16 +159,25 @@ if __name__ == '__main__':
     driver = create_driver()
     start_page(driver)
     click_visible(driver)
-    get_article_list(driver)
+
+    # test_data()
+    # 第一页数据
+    # get_article_list(driver)
+
+    # 0 是第一页，以此类推
+    page: int = 3
+    for i in range(0, page):
+        next_page(driver)
+        if i == (page-1):
+            get_article_list(driver)
 
     # while next_page(driver):
-    #     article_list(driver)
+    #     get_article_list(driver)
 
     for article in article_list:
         open_new_tag(driver, article['href'])
         click_editor_area(driver, article)
-        copy_to_md(article)
+        copy_to_md(article, page)
 
     input("按任意键结束")
     driver.close()
-
